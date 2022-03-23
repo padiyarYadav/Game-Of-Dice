667c130c2f0c33eb6c7e4c0faf54b28edccac8ba
Yadav padiyar M <yadavmpadiyar@gmail.com>
2022-03-23 23:58:23 +0530

    Code cleanup

--- src/PlayGame/utility/CommonConstants.java
+++ src/PlayGame/utility/CommonConstants.java
@@ -1,13 +1,11 @@
 package PlayGame.utility;
 
 public class CommonConstants {
+//    String Constants
     public static final String PLAYER_NAME="Player-";
     public static final String TOTAL_PLAYER_INP_MSG ="Please provide number of players !!!";
     public static final String TOTAL_PLAYER_INP_ERR_MSG="Not a number";
     public static final String WRONG_PLAYER_NUMBER_MSG= "Minimum 2 players are required";
-    public static final Integer BONUS_TURN_POINT=6;
-    public static final Integer PENALTY_POINT=1;
-    public static final Integer PENALTY_NUMBER_OCCUR_VAL=2;
     public static final String ROLLING_DICE_KEY="r";
     public static final String ROLLING_DICE_KEY_MSG=" please press key "+ROLLING_DICE_KEY+" to roll the dice....";
     public static final String PRESS_KEY_TO_START_MESSAGE= "Please press any key to start the game....";
@@ -21,6 +19,13 @@ public class CommonConstants {
     public static final String ROLLED_NUMBER=" rolled number (";
     public static final String STARS_DESIGN_1="******* ";
     public static final String STARS_DESIGN_2=") *******";
+
+//    Intege constants
+    public static final Integer DICE_MIN_VAL=1;
+    public static final Integer DICE_MAX_VAL=6;
+    public static final Integer BONUS_TURN_POINT=6;
+    public static final Integer PENALTY_POINT=1;
+    public static final Integer PENALTY_NUMBER_OCCUR_VAL=2;
 
 
 
